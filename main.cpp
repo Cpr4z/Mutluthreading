c16f9c6f66da699d05cbc184ab989089e84bf0db
Matvey <matvey.agarkov02@gmail.com>
2023-03-21 22:40:36 +0300

    edited commets and deleted intends

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@
 void generate_random_vector(std::vector<long long>& in, int seed, unsigned long long size)
 {
     srand(seed);
-    //long long size = rand()%100000;
     in.clear();
     in.reserve(size);
     for (unsigned long long i = 0; i < size; ++i)
     {
         in.push_back(rand() % 100);
-        //in.push_back(1);
     }
 }
 
@@ -30,20 +28,18 @@ void print_vector(std::vector<long long>& in)
 
 void single_thread_summ(std::vector<long long>& in, int left, int right, long long& summ)
 {
-    //std::cout << "Started: " << std::this_thread::get_id() << "\n";
     for (int i = left; i <= right; ++i)
     {
         summ += in[i];
     }
-    //std::cout << "Ended: " << std::this_thread::get_id() << "\n";
 }
 
 long long multi_thread_summ(std::vector<long long>& in, int left, int right, int threads_num)
 {
-    long long summ = 0;                         //îáùàÿ ñóììà
-    long long total_size = right - left + 1;  //êîëè÷åñòâî ýëåìåíòîâ â çàäàííîì ó÷àñòêå ìàññèâà
-    long long part_size = total_size / threads_num;    //ðàçìåð îäíîé ÷àñòè
-    long long last_part_size = total_size % threads_num;    //ðàçìåð ïîñëåäíåé ÷àñòè, åñëè íå äåëèòñÿ íàöåëî
+    long long summ = 0;                       
+    long long total_size = right - left + 1; 
+    long long part_size = total_size / threads_num;
+    long long last_part_size = total_size % threads_num;
 
     std::vector<std::thread> threads;
 
@@ -56,23 +52,20 @@ long long multi_thread_summ(std::vector<long long>& in, int left, int right, int
     {
         int current_left = part_size * i;
         int current_right = (part_size * (i + 1)) - 1;
-        //std::cout << "Thread # " << i << "  left = " << current_left << "    right = " << current_right << "\n";  //debug
-        threads.push_back(std::thread(single_thread_summ, std::reference_wrapper<std::vector<long long> >(in), current_left, current_right, std::reference_wrapper<long long>(partial_summs[i])));
-        //threads[i].join();    //åñëè äæîéíèòü çäåñü, òî ìíîãîïîòî÷íîñòè íå áóäåò, ïîòîêè áóäóò âûïîëíÿòüñÿ äðóã çà äðóãîì
+        threads.push_back(std::thread(single_thread_summ, std::reference_wrapper<std::vector<long long> >(in),
+         current_left, current_right, std::reference_wrapper<long long>(partial_summs[i])));
     }
 
-    //*
+    
     for (int i = 0; i < threads_num; ++i)
     {
-        threads[i].join();      //à åñëè çäåñü, òî ïîòîêè áóäóò âûïîëíÿòüñÿ àñèíõðîííî
+        threads[i].join();     
     }
-    //*/
+
 
     if (last_part_size != 0)
     {
         int last_left = right - last_part_size + 1;
-        //std::cout << "Last left: " << last_left << "\n";
-
         threads.push_back(std::thread(single_thread_summ, std::reference_wrapper<std::vector<long long> >(in), last_left, right, std::reference_wrapper<long long>(partial_summs[threads_num])));
         threads[threads_num].join();
     }
@@ -87,12 +80,8 @@ int main()
 {
     std::vector<long long> v;
     long long summ = 0;
-    //int threads_num = 5;
     int randomseed = 16;
     generate_random_vector(v, randomseed, 5000000);
-
-    //print_vector(v);
-
     std::cout << "\n\n\n";
     auto start1 = std::chrono::high_resolution_clock::now();
     single_thread_summ(v, 0, v.size() - 1, summ);
@@ -101,9 +90,7 @@ int main()
     std::cout << "Single thread result: " << summ << "   array size: " << v.size() << "\n";
     std::cout << "Elapsed time = " << elapsed_time1.count() << " sec\n";
     std::cout << "\n\n\n";
-
-    //std::cout << multi_thread_summ(v,0,v.size()-1,threads_num) << "\n";
-
+    
     for (int i = 1; i < 100; ++i)
     {
         auto start = std::chrono::high_resolution_clock::now();
@@ -112,7 +99,5 @@ int main()
         std::chrono::duration<double> elapsed_time = stop - start;
         std::cout << "Elapsed time = " << elapsed_time.count() << " sec\n";
     }
-
-
     return 0;
 }
